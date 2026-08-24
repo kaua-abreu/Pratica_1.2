f0764c581b64835a7865526316db0ea6b9adc846
kaua-abreu <kaua808player@gmail.com>
2026-08-24 09:50:36 -0300

    script turismo - etapa 1

--- script_turismo_branches.R
+++ script_turismo_branches.R
@@ -1,6 +1,15 @@
-# Etapa 1: Leitura e manipulação dos dados na branch "main"
 # Leitura e verificação do banco
 # Atribuindo legendas dos rótulos das variáveis qualitativas
+
+# Etapa 1: Leitura e manipulação dos dados na branch “main”
+# Leitura e verificação do banco
+dados = read.csv("dados_turismo.csv", header = T, sep = ";")
+str(dados)
+summary(dados)
+
+# Atribuindo legendas dos rótulos das variáveis qualitativas
+dados$sexo = factor(dados$sexo, levels = c(1, 2), labels = c("masculino", "feminino"))
+dados$estabelecimento = factor(dados$estabelecimento, levels = c("P", "H"), labels = c("pousada", "hotel"))
 
 # Etapa 2: Análise descritiva de dados na branch "analise-descritiva"
 # Tabelas e gráficos de distribuição de frequências das variáveis qualitativas
